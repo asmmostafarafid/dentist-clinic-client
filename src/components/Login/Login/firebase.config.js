d2d717e86762147f6452d6cd6a00700a232b5377
rafid365.info@gmail.com <rafid365.info@gmail.com>
2021-07-01 20:10:32 +0600

    first commit

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBHCJ1WeMY1vFIH26hWJFW9aMgV0bhCdtw",
+  authDomain: "doctors-portal-3feb6.firebaseapp.com",
+  projectId: "doctors-portal-3feb6",
+  storageBucket: "doctors-portal-3feb6.appspot.com",
+  messagingSenderId: "673174060247",
+  appId: "1:673174060247:web:a0187787e3c3d324f64492"
+};
+  export default firebaseConfig;
